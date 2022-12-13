3a97d12811ec754fbce6cfa4871f75e9909fcf72
Leonel Herrera <leoherre@usc.edu>
2022-12-13 00:13:39 +0000

    Initial commit for hw6

---
+++ hash.h
@@ -0,0 +1,60 @@
+#ifndef HASH_H
+#define HASH_H
+
+#include <iostream>
+#include <cmath>
+#include <random>
+#include <chrono>
+
+typedef std::size_t HASH_INDEX_T;
+
+struct MyStringHash {
+    HASH_INDEX_T rValues[5] { 983132572, 1468777056, 552714139, 984953261, 261934300 };
+    MyStringHash(bool debug = true)
+    {
+        if(false == debug){
+            generateRValues();
+        }
+    }
+    // hash function entry point (i.e. this is h(k))
+    HASH_INDEX_T operator()(const std::string& k) const
+    {
+       HASH_INDEX_T index = 0;    
+        return index;
+
+    }
+
+    // A likely helper function is to convert a-z,0-9 to an integral value 0-35
+    HASH_INDEX_T letterDigitToNumber(char letter) const
+    {
+        //variable of type to return 
+        HASH_INDEX_T toNum;
+        //within alphabet and have to check for capatliazation 
+        if(std::isalpha(letter)){
+            toNum = std::tolower(letter);
+            toNum -= 97;
+        }
+        //simple conversion 
+        else{
+            toNum = letter;
+            toNum-= 22;
+        }
+        return toNum;
+    }
+
+    // Code to generate the random R values
+    void generateRValues()
+    {
+        // obtain a seed from the system clock:
+        unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+        std::mt19937 generator (seed);  // mt19937 is a standard random number generator
+
+        // Simply call generator() [it has an operator()] to get another random number
+        for(int i{ 0 }; i < 5; ++i)
+        {
+            rValues[i] = generator();
+        }
+    }
+};
+
+#endif
